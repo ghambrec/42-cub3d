8a85f029f77bc2181042a4fd941d938ea2f3d301
Gabriel Hambrecht <ghambrec@2-C-9.42heilbronn.de>
2025-09-08 16:41:55 +0200

    length player dir adjusted

--- src/minimap/paint_direction.c
+++ src/minimap/paint_direction.c
@@ -6,8 +6,8 @@ void	paint_direction(t_game *game)
 	t_position2D	pos2D;
 	t_position		pos;
 
-	pos2D.x = game->player.pos.x + game->player.dir.x;
-	pos2D.y = game->player.pos.y + game->player.dir.y;
+	pos2D.x = game->player.pos.x + game->player.dir.x * 0.5;
+	pos2D.y = game->player.pos.y + game->player.dir.y * 0.5;
 	pos.x = pos2D.x * game->minimap.tile_size + game->minimap.off_x;
 	pos.y = pos2D.y * game->minimap.tile_size + game->minimap.off_y;
 	put_pixel_safe(game->img_minimap, pos.x, pos.y, CMM_DIR);
