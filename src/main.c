3204c011edc31434383fb0bf0a853f67037b1803
robin stumpf <127445060+rstumpf01@users.noreply.github.com>
2025-11-30 14:34:50 +0100

    deletetd unnecassary function

--- src/main.c
+++ src/main.c
@@ -9,6 +9,13 @@ int	main(int argc, char **argv)
 	game.map.map_name = argv[1];
 	map_validation(&game);
 
+	printf("%s", game.map.floor_color);
+	printf("%s", game.map.ceiling_color);
+	printf("%s", game.texture_path.east);
+	printf("%s", game.texture_path.north);
+	printf("%s", game.texture_path.west);
+	printf("%s", game.texture_path.south);
+	
 	init_mlx(&game);
 	//render_game(&game);
 	//init_hooks(&game);
