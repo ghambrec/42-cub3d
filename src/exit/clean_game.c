c0f2d92a7fd3d6c502e6a1d85d59a435a4022573
robin stumpf <127445060+rstumpf01@users.noreply.github.com>
2025-11-30 14:47:57 +0100

    merge try

--- src/exit/clean_game.c
+++ src/exit/clean_game.c
@@ -4,9 +4,7 @@ void	clean_game(t_game *game)
 {
 	(void)game;
 	ft_putendl_fd("TODO --> clean_game.c", STDOUT_FILENO);
-
-	//delete textures hier nicht vergessen
-
-	//mlx terminate hier noch richtig mit einbauen
+	free(game->map.map_string);
+	ft_str2d_free(game->map.map2d);
 	mlx_terminate(game->mlx);
 }
