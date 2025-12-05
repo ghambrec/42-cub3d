659971c367d4fd101033dae77e13a572eeb6dc9d
ghambrecht <gabriel.hambrecht@gmail.com>
2025-12-05 12:33:46 +0100

    norm completed

--- src/parsing/check_dublicates.c
+++ src/parsing/check_dublicates.c
@@ -1,52 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_dublicates.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghambrec <ghambrec@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/05 12:33:14 by ghambrec          #+#    #+#             */
+/*   Updated: 2025/12/05 12:33:33 by ghambrec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
-void    parse_rgbs(char *string, t_game *game)
+void	parse_rgbs(char *string, t_game *game)
 {
-    if (string[0] == 'F' && string[1] == ' ')
+	if (string[0] == 'F' && string[1] == ' ')
 	{
-        if (game->map.floor_color != 0)
-            exit_failure(game, "dublicates not allowed");
+		if (game->map.floor_color != 0)
+			exit_failure(game, "dublicates not allowed");
 		game->map.floor_color = parse_color(string, game);
 		return ;
 	}
 	if (string[0] == 'C' && string[1] == ' ')
 	{
 		if (game->map.ceiling_color != 0)
-            exit_failure(game, "dublicates not allowed");
+			exit_failure(game, "dublicates not allowed");
 		game->map.ceiling_color = parse_color(string, game);
 		return ;
 	}
 }
 
-static void check_dublicate(char *string, t_game *game, char *path)
+static void	check_dublicate(char *string, t_game *game, char *path)
 {
-    if (string != NULL)
+	if (string != NULL)
 	{
 		free(path);
-        exit_failure(game, "dublicates not allowed");
+		exit_failure(game, "dublicates not allowed");
 	}
 }
 
-void    parse_textures(char *string, char *path, t_game *game)
+void	parse_textures(char *string, char *path, t_game *game)
 {
-    if (string[0] == 'N' && string[1] == 'O')
-    {
-        check_dublicate(game->texture_path.north, game, path);
+	if (string[0] == 'N' && string[1] == 'O')
+	{
+		check_dublicate(game->texture_path.north, game, path);
 		game->texture_path.north = ft_strdup(path);
-    }
+	}
 	else if (string[0] == 'S' && string[1] == 'O')
-    {
-        check_dublicate(game->texture_path.south, game, path);
+	{
+		check_dublicate(game->texture_path.south, game, path);
 		game->texture_path.south = ft_strdup(path);
-    }
+	}
 	else if (string[0] == 'W' && string[1] == 'E')
-    {
-        check_dublicate(game->texture_path.west, game, path);
+	{
+		check_dublicate(game->texture_path.west, game, path);
 		game->texture_path.west = ft_strdup(path);
-    }
+	}
 	else if (string[0] == 'E' && string[1] == 'A')
-    {
-        check_dublicate(game->texture_path.east, game, path);
+	{
+		check_dublicate(game->texture_path.east, game, path);
 		game->texture_path.east = ft_strdup(path);
-    }
+	}
 }
